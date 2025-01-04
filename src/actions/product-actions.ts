19f2f199c18b60c1a9acb721e73383ee1b240732
Evandro Calado <evandrocalado07@gmail.com>
2025-01-04 00:22:09 -0300

    feat: integrate Neon database with Prisma and extend PrismaClient for custom result transformation

--- src/actions/product-actions.ts
+++ src/actions/product-actions.ts
@@ -1,11 +1,9 @@
 'use server';
 
+import { prisma } from '@/db/prisma';
 import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants';
-import { PrismaClient } from '@prisma/client';
 
 export const getLatestProducts = async () => {
-  const prisma = new PrismaClient();
-
   const products = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
